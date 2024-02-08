1e615dc62d46b5c3b85099e1d396ab57b47a938c
Youtpout <EddyBoughioul@hotmail.fr>
2024-02-08 20:15:24 +0100

    add interface contract service

---
+++ SonicBoomService/Interfaces/IContractService.cs
@@ -0,0 +1,15 @@
+﻿using SonicBoomDto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SonicBoomService.Interfaces
+{
+    public interface IContractService
+    {
+        Task Add(string addressUser, int idProject, AccountDto newAccount);
+        Task<List<AccountDto>> GetContracts(string addressUser, int idProject);
+    }
+}
